8d9a4eb3ed48a6900951e93995eaffbb2d5ea442
xanderwessels <x.b.wessels@student.tue.nl>
2016-12-23 22:54:07 +0100

    test webstorm

--- list/views.py
+++ list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.filter(deadline_date__lte=timezone.now()).order_by('deadline_date')
+    return render(request, 'list/todo_list.html', {'todos': todos})
