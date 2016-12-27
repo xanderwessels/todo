5415dd0a6af1346c32b1bcab74dc73000f91547d
xanderwessels <x.b.wessels@student.tue.nl>
2016-12-27 16:32:36 +0100

    fixed static files?

--- todolist/settings.py
+++ todolist/settings.py
@@ -121,7 +121,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
-)
