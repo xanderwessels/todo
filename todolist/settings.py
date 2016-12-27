8be307dae45919380db85ade7c2f128229692f49
xanderwessels <x.b.wessels@student.tue.nl>
2016-12-27 16:30:30 +0100

    better

--- todolist/settings.py
+++ todolist/settings.py
@@ -123,5 +123,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
