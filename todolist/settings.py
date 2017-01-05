c218fb2e39e24e3fe2b49817f6d59ba096e6fab8
xanderwessels <x.b.wessels@student.tue.nl>
2017-01-05 16:04:38 +0100

    heroku database test

--- todolist/settings.py
+++ todolist/settings.py
@@ -78,6 +78,13 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES['default'] =  dj_database_url.config()
 
 
