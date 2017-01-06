42fd11d36b9e32021d4864a3c1432cd99a42709c
xanderwessels <x.b.wessels@student.tue.nl>
2017-01-06 13:56:07 +0100

    extended the API

--- list/serializers.py
+++ list/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from list.models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Item
-        fields = ('id', 'title', 'text', 'image', 'archived', 'created_date', 'archived_date', 'color')
+        fields = ('id', 'title', 'text', 'image', 'archived', 'archived_date', 'created_date' )
